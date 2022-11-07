f97cae2f748189d086f6bff2048c2379598846ff
Jordan Reed <reed5204@vandals.uidaho.edu>
2022-11-06 21:37:36 -0800

    added code to query the database and create a graph. added query to load data from database for the table.

--- degree_overview.php
+++ degree_overview.php
@@ -29,6 +29,36 @@ $result = $mysqli->query($sql);
 $answer = $mysqli->query($sqll);
 $mysqli->close();
 
+require("php_scripts/db_connection.php");
+// query for course table
+$con = OpenCon();
+$qry = "select * from classes";
+
+$rs = mysqli_query($con, $qry);
+
+$size = 0;
+while($row = mysqli_fetch_array($rs, MYSQLI_ASSOC))
+{
+    $data[$size] = $row;
+    $size++;
+}
+
+// query for graph
+$qry = "select prereq.prereqID, p.department as pdept, p.classNumber as pnum, p.name as pname, p.credits as pcred, prereq.classID, c.department, c.classNumber, c.name, c.credits
+        from prereq 
+        join classes as p on prereq.prereqID=p.classID
+        join classes as c on prereq.classID=c.classID";
+
+$rs = mysqli_query($con, $qry);
+
+$gSize = 0;
+while($row = mysqli_fetch_array($rs, MYSQLI_ASSOC))
+{
+    $graph[$gSize] = $row;
+    $gSize++;
+}
+CloseCon($con);
+
 ?>
 
 <!DOCTYPE html>  
@@ -48,10 +78,13 @@ $mysqli->close();
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 
         <!-- flowchart -->
-        <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/lib/createjs-2015.05.21.min.js"></script>
+        <!-- <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/lib/createjs-2015.05.21.min.js"></script>
         <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/src/flow.js"></script>
         <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/src/flowitem.js"></script>
-        <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/src/flowconnector.js"></script>
+        <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/src/flowconnector.js"></script> -->
+
+        <script type="text/javascript" src="flowjs/lib/createjs-2015.05.21.min.js"></script>
+        <script type="text/javascript" src="flowjs/flow.min.js"></script>
 
         <style>
             body {
@@ -89,30 +122,12 @@ $mysqli->close();
     </head>
 
     <body>  
-        <!-- Navbar -->
-        <nav class="navbar navbar-dark bg-dark navbar-expand-lg nav-pills" style="background-color:black !important">
-            <span class="navbar-brand mb-0 h1" style="color:rgb(231, 200, 25)"><strong>ProViso Online Advising</strong></span>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <div class="navbar-nav">
-                    <a href="index.html" class="nav-item nav-link my-nav-item">Home</a>
-                    <a href="degree_overview.html" class="active nav-item  nav-link my-nav-item">Degree Overview</a>
-                    <a href="career.html" class="nav-item nav-link my-nav-item">Career Overview</a>
-
-                    <a href= Class_Reg.php class="nav-item nav-link my-nav-item" >Course Register</a>
-                    <a href="#" class="nav-item nav-link my-nav-item disabled">Course Mapper</a>
-                    <a href="#" class="nav-item nav-link my-nav-item disabled">Course outcomes</a>
-                    <a href="sign_up.html" class="nav-item nav-link my-nav-item">Register</a>
-					 <a href="#" class="nav-item nav-link my-nav-item">Logout</a>
-                </div>
-            </div>
-
-            
-        </nav>
+        <?php 
+            include("navbar.php");
+            include("header.php");
+            Navbar("degree");
+            NameHeader("Jane Doe");
+        ?>
 
 
         <header style="padding:80px; text-align: center;">
@@ -146,48 +161,46 @@ $mysqli->close();
         </div>
         <div class="container-fluid" style="width: 80%">
             <div class="card my-card shadow p-3 mb-5 bg-white rounded">
-                
                 <div class="card-body">
                     <h2>Courses</h2>
-                    <table class="table table-hover">
-                        <div>
-                            Legend:
-                            <i class="fa fa-square-o" aria-hidden="true"></i> Not Completed
-                            <i class="fa fa-check-square-o" aria-hidden="true"></i> Completed
-                            <i class="fa fa-plus-square-o" aria-hidden="true"></i> Added by Career Goal
-                        </div>
+                    <div>
+                        Legend:
+                        <i class="fa fa-square-o" aria-hidden="true"></i> Not Completed
+                        <i class="fa fa-check-square-o" aria-hidden="true"></i> Completed
+                        <i class="fa fa-plus-square-o" aria-hidden="true"></i> Added by Career Goal
+                    </div>
 						
-						   <table class="table bg-white" id="myTable">
-                <thead class="bg-dark text-light">
-                    <tr>
-                        <th></th>
-                        <th>Course Number</th>
-                        <th>Course Name</th>
-                        <th>Skills</th>
-                     
-                    </tr>
-                </thead>
-                <tbody>
-               <?php
-				// LOOP TILL END OF DATA
-				while($rows=$result->fetch_assoc())
-				{
+                    <table class="table bg-white" id="myTable">
+                        <thead class="bg-dark text-light">
+                            <tr>
+                                <th></th>
+                                <th>Course Number</th>
+                                <th>Course Name</th>
+                                <th class="table-skills">Skills</th>
+                            
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                // LOOP TILL END OF DATA
+                                // while($rows=$result->fetch_assoc())
+                                for($i=0; $i<$size; $i++)
+                                {
+			                ?>
+                            <tr>
+                                <td ><i class="fa fa-check-square-o" aria-hidden="true"></i></td>
+                                <td><?php echo $data[$i]['classNumber']; //$rows['Course_Number'];?></td>
+                                <td><?php echo $data[$i]['name']." (".$data[$i]['credits'].")";//$rows['Course_Name'];?></td>
+                                <td>
+                                    <span class="badge badge-info" style="background-color:black">
+                                        <?php echo 'skills'//$rows['Skill'];?>
+                                    </span>
+                                </td>
 
-
-			?>
-                    <tr>
-                        <td ><i class="fa fa-check-square-o" aria-hidden="true"></i></td>
-						<td><?php echo $rows['Course_Number'];?></td>
-						<td><?php echo $rows['Course_Name'];?></td>
-						<td><?php echo $rows['Skill'];?></td>
-				
-                    </tr>
-			
-           				
-		<?php
-				}
-		
-			?>	
+                            </tr>
+		                    <?php
+				                }
+		                    ?>	
 						<!--
                         <thead class="table-dark">
                             <tr>
@@ -276,14 +289,26 @@ $mysqli->close();
     // query what that class is a prereq of
     // start building chart
 
-    var e = [
-        [{id: "CS 121", next: ["CS 210", "CS 240", "CS 270"]}], //{id: "z1", empty: true, next: undefined}, {id: "z1", empty: true, next: undefined}],
-        [{id: "CS 210", next: ["CS 383"]}, {id: "CS 240", next: ["CS 383"]},  {id: "CS 270", next: ["CS 383"]}],
-        [{id: "CS 383", next: undefined}]//, {id: "Software Engineering", next: undefined}, {{id: "z3", empty: true, next: undefined}],
-    ];
+    // var e = [
+    //     [{id: "CS 121", next: ["CS 210", "CS 240", "CS 270"]}], //{id: "z1", empty: true, next: undefined}, {id: "z1", empty: true, next: undefined}],
+    //     [{id: "CS 210", next: ["CS 383"]}, {id: "CS 240", next: ["CS 383"]},  {id: "CS 270", next: ["CS 383"]}],
+    //     [{id: "CS 383", next: undefined}]//, {id: "Software Engineering", next: undefined}, {{id: "z3", empty: true, next: undefined}],
+    // ];
 
     // new flowjs("demo", d).draw();
-    new flowjs("demo", e).draw();
+    // new flowjs("demo", e).draw();
+
+    var passedArray = 
+    <?php echo json_encode($graph); ?>;
+
+    var g = new flowjs.DiGraph();
+    for(i=0; i<passedArray.length; i++)
+    {
+        g.addPaths([
+            [passedArray[i]['pnum'], passedArray[i]['classNumber']]
+        ]);
+    }
+    new flowjs.DiFlowChart("demo", g).draw();
 
 
 </script>

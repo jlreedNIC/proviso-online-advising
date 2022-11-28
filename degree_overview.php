38471e1ac28e397f0ae90cc77519f70fe8e64a22
Jordan Reed <reed5204@vandals.uidaho.edu>
2022-11-28 10:10:23 -0800

    formatting changes

--- degree_overview.php
+++ degree_overview.php
@@ -88,20 +88,6 @@ CloseCon($con);
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
 
-        <!-- flowchart -->
-        <!-- <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/lib/createjs-2015.05.21.min.js"></script>
-        <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/src/flow.js"></script>
-        <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/src/flowitem.js"></script>
-        <script src="https://www.cssscript.com/demo/dynamic-flow-chart-library-with-createjs-flowjs/src/flowconnector.js"></script> -->
-
-        <!-- the following script will make graph work on Riley's comp -->
-        <script type="text/javascript" src="flowjs-master/lib/createjs-2015.05.21.min.js"></script>
-        <script type="text/javascript" src="flowjs-master/flow.min.js"></script>
-
-        <!-- the following script will make graph work on Jordan's comp -->
-        <script type="text/javascript" src="flowjs/lib/createjs-2015.05.21.min.js"></script>
-        <script type="text/javascript" src="flowjs/flow.min.js"></script>
-
         <style>
             body {
                 background-color: white;
@@ -216,19 +202,6 @@ CloseCon($con);
             </div>
         </div>
 
-        <!-- <div class="container-fluid" style="width:80%">
-            <div class="card my-card shadow p-3 mb-5 bg-white rounded">
-                <div class="card-body">
-                    <h2> Sample Course Layout</h2>
-                    <canvas id="demo" width="500" height="300">
-                    </canvas>
-                  
-                </div>
-            </div>
-        </div> -->
-
-
-
         <div class="container-fluid" style="width:80%">
             <div class="card my-card shadow p-3 mb-5 bg-white rounded">
                 <div class="card-body">
@@ -240,7 +213,6 @@ CloseCon($con);
             </div>
         </div>
     
-        
         <?php
             include('templates/footer.php');
             Footer();
@@ -253,33 +225,4 @@ CloseCon($con);
 <script>
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {return new bootstrap.Tooltip(tooltipTriggerEl)})
-</script>
-
-<script>
-    // query for each class
-    //
-    // query what that class is a prereq of
-    // start building chart
-
-    // var e = [
-    //     [{id: "CS 121", next: ["CS 210", "CS 240", "CS 270"]}], //{id: "z1", empty: true, next: undefined}, {id: "z1", empty: true, next: undefined}],
-    //     [{id: "CS 210", next: ["CS 383"]}, {id: "CS 240", next: ["CS 383"]},  {id: "CS 270", next: ["CS 383"]}],
-    //     [{id: "CS 383", next: undefined}]//, {id: "Software Engineering", next: undefined}, {{id: "z3", empty: true, next: undefined}],
-    // ];
-
-    // new flowjs("demo", d).draw();
-    // new flowjs("demo", e).draw();
-
-    // var passedArray = <?php //echo json_encode($graph); ?>;
-
-    // var g = new flowjs.DiGraph();
-    // for(i=0; i<passedArray.length; i++)
-    // {
-    //     g.addPaths([
-    //         [passedArray[i]['pnum'], passedArray[i]['Course_Num']]
-    //     ]);
-    // }
-    // new flowjs.DiFlowChart("demo", g).draw();
-
-
 </script>
--- index.php
+++ index.php
@@ -26,7 +26,6 @@ $sql = "SELECT Degree_Type, Degree_Name, Description, grade, firstName, lastName
         where user_career.User_ID = ".$_SESSION['userID']."";
 
 $result = $mysqli->query($sql);
-print_r($result);
 
 $size = 0;
 while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
@@ -34,7 +33,6 @@ while($row = mysqli_fetch_array($result, MYSQLI_ASSOC))
     $userInfo[$size] = $row;
     $size++;
 }
-print_r($userInfo);
 
 $mysqli->close();
 
@@ -43,41 +41,39 @@ $mysqli->close();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Online Advising</title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Online Advising</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link href="mycss.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="mycss.css" rel="stylesheet">
 
-  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
 
-  <style>
-    body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
-    .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
-    .fa-book,.fa-desktop {font-size:200px}
-    body {
-      background-color: white;
-    }
-	header{
+    <style>
+        body,h1,h2,h3,h4,h5,h6 {font-family: "Lato", sans-serif}
+        .w3-bar,h1,button {font-family: "Montserrat", sans-serif}
+        .fa-book,.fa-desktop {font-size:200px}
+        body {
+            background-color: white;
+        }
+        header{
 
-        width: 100%;
-        height: 50%;
-        padding: 0 0 50%;
-        border: 1px solid;
-        background-size: 50%;
-        background-repeat: no-repeat;
-        background-size: cover;
-        margin-left: auto;
-        margin-right: auto;
-     }
-
-	
-  </style>
+            width: 100%;
+            height: 50%;
+            padding: 0 0 50%;
+            border: 1px solid;
+            background-size: 50%;
+            background-repeat: no-repeat;
+            background-size: cover;
+            margin-left: auto;
+            margin-right: auto;
+        }
+    </style>
 </head>
 
 <body>  
@@ -86,74 +82,69 @@ $mysqli->close();
     include('templates/navbar.php');
     Navbar("home");
 
-    // do we need this or keep the name below on the dashboard page
-    // include('templates/header.php');
-    // NameHeader($userInfo[0]['firstName']." ".$userInfo[0]['lastName']);
 ?>
 
 <!-- Header <img class="center-fit" src="img/notebook.jpg" >-->
 <header class="w3-container w3-cyan w3-center " style="padding:128px 16px; background-image: url('img/ccc.jpg')  !important">
-  <div style="background-color: black; width:50%; border: 2px solid white" class="container-fluid">
-    <h1 class="w3-margin w3-jumbo" style="color:white">Dashboard</h1>
-    <p style="color:white" class="w3-xlarge"> <?php echo $_SESSION['role']; ?> </p>
-    <p style="color:white" class="w3-large">
-      <?php echo $_SESSION['firstName']." ".$_SESSION['lastName']; ?> <br>
-      <?php echo $userInfo[0]['grade']; ?>
-    </p>
-  </div>
+    <div style="background-color: black; width:30%; border: 2px solid white" class="container-fluid">
+        <h1 class="w3-margin w3-jumbo" style="color:white">Dashboard</h1>
+        <p style="color:white" class="w3-xlarge"> <?php echo $_SESSION['role']; ?> </p>
+        <p style="color:white" class="w3-large">
+            <?php echo $_SESSION['firstName']." ".$_SESSION['lastName']; ?> <br>
+            <?php echo $userInfo[0]['grade']; ?>
+        </p>
+    </div>
 </header>
 
 <!-- First Grid -->
 <div class="w3-row-padding w3-padding-64 w3-container">
-  <div class="w3-content">
+    <div class="w3-content">
+        <div class="w3-twothird">
+            <!-- degree info  -->
+            <h1><?php echo $userInfo[0]['Degree_Type']." ".$userInfo[0]['Degree_Name']; ?>  </h1>
+            <p class="w3-text-grey w3-padding-32"><?php echo $userInfo[0]['Description']; ?></p>
+        </div>
 
-    <div class="w3-twothird">
-      <!-- degree info  -->
-      <h1><?php echo $userInfo[0]['Degree_Type']." ".$userInfo[0]['Degree_Name']; ?>  </h1>
-      <p class="w3-text-grey w3-padding-32"><?php echo $userInfo[0]['Description']; ?></p>
+        <div class="w3-third w3-center">
+            <i class="fa fa-book w3-padding-64 w3-text-black"></i>
+        </div>
     </div>
-
-    <div class="w3-third w3-center">
-      <i class="fa fa-book w3-padding-64 w3-text-black"></i>
-    </div>
-
-  </div>
 </div>
 
 <!-- Second Grid -->
 <div class="w3-row-padding w3-light-grey w3-padding-64 w3-container">
-  <div class="w3-content">
+    <div class="w3-content">
 
-    <div class="w3-third w3-center">
-      <i class="fa fa-desktop w3-padding-64 w3-text-black w3-margin-right"></i>
+        <div class="w3-third w3-center">
+            <i class="fa fa-desktop w3-padding-64 w3-text-black w3-margin-right"></i>
+        </div>
+
+        <div class="w3-twothird">
+            <h1><?php echo $userInfo[0]['Position_Name']; ?> </h1>
+            <p class="w3-text-grey w3-padding-32"><?php echo $userInfo[0]['Des']; ?></p>
+        </div>
+
     </div>
-
-    <div class="w3-twothird">
-      <h1><?php echo $userInfo[0]['Position_Name']; ?> </h1>
-      <p class="w3-text-grey w3-padding-32"><?php echo $userInfo[0]['Des']; ?></p>
-    </div>
-
-  </div>
 </div>
 
 <!-- Third Grid -->
 <div class="w3-row-padding w3-padding-64 w3-container">
-  <div class="w3-content">
+    <div class="w3-content">
 
-    <div class="w3-twothird">
-      <h1>Career graph</h1>
-      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+        <div class="w3-twothird">
+            <h1>Career graph</h1>
+            <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
 
-      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
+                occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+        <div class="w3-third w3-center">
+            <i class="fa fa-book w3-padding-64 w3-text-black"></i>
+        </div>
+
     </div>
-
-    <div class="w3-third w3-center">
-      <i class="fa fa-book w3-padding-64 w3-text-black"></i>
-    </div>
-
-  </div>
 </div>
 
 <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">

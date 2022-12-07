411ea6cecdddea79f24267313a21ee6962334563
Riley Walsh <98550098+wals0909@users.noreply.github.com>
2022-12-06 16:51:27 -0800

    remove chage career link, edit dashboard

--- index.php
+++ index.php
@@ -127,28 +127,12 @@ $mysqli->close();
     </div>
 </div>
 
-<!-- Third Grid -->
-<div class="w3-row-padding w3-padding-64 w3-container">
-    <div class="w3-content">
-
-        <div class="w3-twothird">
-            <h1>Career graph</h1>
-            <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
-
-            <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-                occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div>
-
-        <div class="w3-third w3-center">
-            <i class="fa fa-book w3-padding-64 w3-text-black"></i>
-        </div>
 
     </div>
 </div>
 
 <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
-    <h1 class="w3-margin w3-xlarge">Quote of the day: live life</h1>
+    <h1 class="w3-margin w3-xlarge">"Successful and unsuccessful people do not vary greatly in their abilities. They vary in there desires to reach there potential" -John Maxwell</h1>
 </div>
  
 
@@ -157,7 +141,6 @@ $mysqli->close();
 include('templates/footer.php');
 Footer();
 ?>
-
 
 
 <script>
